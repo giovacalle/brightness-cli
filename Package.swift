8ee65a4f34ce3b2db4c3fa47c432f2c846363b4f
giovacalle <at.giova.dev@gmail.com>
2025-06-28 15:51:43 +0200

    feat: add release workflow

--- Package.swift
+++ Package.swift
@@ -4,22 +4,22 @@ import PackageDescription
 let package = Package(
     name: "brightness-cli",
     platforms: [
-        .macOS(.v13),
+        .macOS(.v13)
     ],
     dependencies: [
         // DDC library for Apple Silicon
         .package(url: "https://github.com/waydabber/AppleSiliconDDC.git", branch: "main"),
         // Argument parser for CLI
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0")
     ],
     targets: [
         .executableTarget(
             name: "brightness-cli",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "AppleSiliconDDC", package: "AppleSiliconDDC"),
+                .product(name: "AppleSiliconDDC", package: "AppleSiliconDDC")
             ],
-            path: "Sources",
-        ),
+            path: "Sources"
+        )
     ]
 )
